9aa1d6432e1e187466d565b07f13c96a59a4d71f
sherlock1879 <sherlockholmes0806@hotmail.com>
2023-06-11 11:53:02 +0800

    hello2

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,18 +1,10 @@
-# 在该文件NeuralNetwork类中定义你的模型 
-# 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
-
 import os
-
-os.system("sudo pip3 install torch")
-os.system("sudo pip3 install torchvision")
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
-
 from torch.utils.data import DataLoader
-
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class ResNet(nn.Module):
@@ -27,8 +19,6 @@ class ResNet(nn.Module):
 
 
 def read_data():
-    # 这里可自行修改数据预处理，batch大小也可自行调整
-    # 保持本地训练的数据读取和这里一致
     dataset_train = torchvision.datasets.CIFAR10(
         root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor()
     )
@@ -40,12 +30,15 @@ def read_data():
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 
-def train_model(model, data_loader_train, data_loader_val, num_epochs=100, learning_rate=0.0001):
+def train_model(model, data_loader_train, data_loader_val, num_epochs=100, learning_rate=0.1):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+    scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=3, verbose=True)
+
+    best_val_acc = 0.0
 
     for epoch in range(num_epochs):
         model.train()
@@ -88,23 +81,29 @@ def train_model(model, data_loader_train, data_loader_val, num_epochs=100, learn
             val_loss = val_loss / len(data_loader_val.dataset)
             val_acc = val_correct.double() / len(data_loader_val.dataset)
 
+        scheduler.step(val_acc)
+
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            parent_dir = os.path.dirname(current_dir)
+            torch.save(model.state_dict(), parent_dir + '/pth/model.pth')
+
         print(
             f"Epoch {epoch + 1}/{num_epochs} - Train Loss: {train_loss:.4f} - Train Acc: {train_acc:.4f} - Val Loss: {val_loss:.4f} - Val Acc: {val_acc:.4f}"
         )
 
     return model
 
-def main():
-    model = ResNet() # 若有参数则传入参数
-    dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
-    model = train_model(model, data_loader_train, data_loader_val)
 
+def main():
+    model = ResNet()
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
-    torch.save(model.state_dict(), parent_dir + '/pth/model.pth')
-
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
 
-if __name__ == '__main__':
-    model = main()
+
+model = ResNet()
+dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
+model = train_model(model, data_loader_train, data_loader_val)
