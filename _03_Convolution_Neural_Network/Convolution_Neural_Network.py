0d5a0d049eef3eba6ccc4ddc9c1823fda5058be0
sherlock1879 <sherlockholmes0806@hotmail.com>
2023-06-12 11:03:18 +0800

    webtest

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -11,15 +11,75 @@ from torch.utils.data import DataLoader
 import torchvision.models as models
 
 
-class ResNet(nn.Module):
-    def __init__(self):
-        super(ResNet, self).__init__()
-        self.resnet = torchvision.models.resnet18(pretrained=False)
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, 10)
+def conv3x3(in_channels, out_channels, stride=1):
+    return torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+# Define Residual block
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = torch.nn.BatchNorm2d(out_channels)
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = torch.nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
 
     def forward(self, x):
-        return self.resnet(x)
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+# Define ResNet-18
+class ResNet(torch.nn.Module):
+    def __init__(self, block, layers, num_classes):
+        super(ResNet, self).__init__()
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = torch.nn.BatchNorm2d(16)
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.layer1 = self._make_layers(block, 16, layers[0])
+        self.layer2 = self._make_layers(block, 32, layers[1], 2)
+        self.layer3 = self._make_layers(block, 64, layers[2], 2)
+        self.layer4 = self._make_layers(block, 128, layers[3], 2)
+        self.avg_pool = torch.nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = torch.nn.Linear(128, num_classes)
+
+    def _make_layers(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = torch.nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                torch.nn.BatchNorm2d(out_channels))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 
 def read_data():
@@ -35,7 +95,8 @@ def read_data():
 
 
 def main():
-    model = ResNet()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = ResNet(ResidualBlock, [2, 2, 2, 2], num_classes).to(device)
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth',map_location='cpu'))
